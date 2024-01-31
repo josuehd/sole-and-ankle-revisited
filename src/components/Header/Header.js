23636f6ecfb6b9725857ca42a3bd796146727180
Josue Davila <josuedavila90@gmail.com>
2024-01-31 15:26:42 -0600

    Exercise 5: Fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,11 +12,6 @@ import VisuallyHidden from '../VisuallyHidden';
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
@@ -61,9 +56,9 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   border-top: 4px solid ${COLORS.gray[900]};
+  overflow-x: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
@@ -77,7 +72,7 @@ const MainHeader = styled.div`
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
   @media ${QUERIES.tabletAndSmaller} {
     display: none;
